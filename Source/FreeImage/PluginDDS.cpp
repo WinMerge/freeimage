ab21cf75e4c13a734d75595bee9854136a60c2a2
Sergey Fedorov <vital.had@gmail.com>
2024-01-30 06:05:35 +0800

    Fix the build for Big-endian platforms (#17)
    * PluginBMP: fix for Big-endian
    
    * PluginDDS: fix for Big-endian
    
    * ImfFastHuf: backport fix from upstream for 32-bit

--- Source/FreeImage/PluginDDS.cpp
+++ Source/FreeImage/PluginDDS.cpp
@@ -356,6 +356,14 @@ SwapHeader(DDSHEADER *header) {
 	for(int i=0; i<11; i++) {
 		SwapLong(&header->surfaceDesc.dwReserved1[i]);
 	}
+	SwapLong(&header->surfaceDesc.ddspf.dwSize);
+	SwapLong(&header->surfaceDesc.ddspf.dwFlags);
+	SwapLong(&header->surfaceDesc.ddspf.dwFourCC);
+	SwapLong(&header->surfaceDesc.ddspf.dwRGBBitCount);
+	SwapLong(&header->surfaceDesc.ddspf.dwRBitMask);
+	SwapLong(&header->surfaceDesc.ddspf.dwGBitMask);
+	SwapLong(&header->surfaceDesc.ddspf.dwBBitMask);
+	SwapLong(&header->surfaceDesc.ddspf.dwRGBAlphaBitMask);
 	SwapLong(&header->surfaceDesc.ddsCaps.dwCaps1);
 	SwapLong(&header->surfaceDesc.ddsCaps.dwCaps2);
 	SwapLong(&header->surfaceDesc.ddsCaps.dwReserved[0]);
